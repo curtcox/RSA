7390b82cc7d745a650cabf883d3ff4e8451d49ca
Curt Cox <curtcox@gmail.com>
2018-09-30 12:37:50 -0500

    Split RSA generator into separate class.

--- RSA.java
+++ RSA.java
@@ -1,50 +1,20 @@
 import java.math.BigInteger;
-import java.security.SecureRandom;
 
 final class RSA {
-
 
    final BigInteger privateKey;
    final BigInteger publicKey;
    final BigInteger modulus;
 
-   private final static BigInteger one      = new BigInteger("1");
-   private final static SecureRandom random = new SecureRandom();
-
-   private RSA(BigInteger publicKey, BigInteger privateKey, BigInteger modulus) {
+   RSA(BigInteger publicKey, BigInteger privateKey, BigInteger modulus) {
       this.publicKey  = publicKey;
       this.privateKey = privateKey;
       this.modulus    = modulus;
    }
 
-   // generate an N-bit (roughly) public and private key
-   static RSA bitLength(int bits) {
-      int bitLength = bits/2;
-      int certainlyPrime = 100;
-      BigInteger p = new BigInteger(bitLength, certainlyPrime, random);
-      BigInteger q = new BigInteger(bitLength, certainlyPrime, random);
-      BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
-      BigInteger publicKey = generatePublicKey(phi);
-      BigInteger modulus = p.multiply(q);
-      BigInteger privateKey = publicKey.modInverse(phi);
-      return new RSA(publicKey,privateKey,modulus);
+   static RSA bitLength(int i) {
+      return new RSAGenerator(i).generate();
    }
-
-   static BigInteger generatePublicKey(BigInteger phi) {
-      while (true) {
-         BigInteger publicKey = new BigInteger(phi.bitLength(), random);
-         if (suitable(publicKey,phi)) {
-            return  publicKey;
-         }
-      }
-   }
-
-   static boolean suitable(BigInteger publicKey,BigInteger phi) {
-      return  publicKey.compareTo(one) > 0 &&
-              publicKey.compareTo(phi) < 0            &&
-              publicKey.gcd(phi).equals(one);
-   }
-
 
    BigInteger encrypt(BigInteger message) {
       return message.modPow(publicKey, modulus);
@@ -56,9 +26,9 @@ final class RSA {
 
    public String toString() {
       return
-          "public  = " + publicKey  + "\n" +
-          "private = " + privateKey + "\n" +
-          "modulus = " + modulus;
+              "public  = " + publicKey  + "\n" +
+                      "private = " + privateKey + "\n" +
+                      "modulus = " + modulus;
    }
- 
+
 }
---
+++ RSAGenerator.java
@@ -0,0 +1,42 @@
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+final class RSAGenerator {
+
+   private int bits;
+   private static final BigInteger      one = new BigInteger("1");
+   private static final SecureRandom random = new SecureRandom();
+
+   // generate an N-bit (roughly) public and private key
+   RSAGenerator(int bits) {
+      this.bits = bits;
+   }
+
+   RSA generate() {
+      int bitLength = bits/2;
+      int certainlyPrime = 100;
+      BigInteger p = new BigInteger(bitLength, certainlyPrime, random);
+      BigInteger q = new BigInteger(bitLength, certainlyPrime, random);
+      BigInteger phi = (p.subtract(one)).multiply(q.subtract(one));
+      BigInteger publicKey = generatePublicKey(phi);
+      BigInteger modulus = p.multiply(q);
+      BigInteger privateKey = publicKey.modInverse(phi);
+      return new RSA(publicKey,privateKey,modulus);
+   }
+
+   BigInteger generatePublicKey(BigInteger phi) {
+      while (true) {
+         BigInteger publicKey = new BigInteger(phi.bitLength(), random);
+         if (suitable(publicKey,phi)) {
+            return  publicKey;
+         }
+      }
+   }
+
+   boolean suitable(BigInteger publicKey,BigInteger phi) {
+      return  publicKey.compareTo(one) > 0 &&
+              publicKey.compareTo(phi) < 0            &&
+              publicKey.gcd(phi).equals(one);
+   }
+
+}
